6d240024ee4edb4df5a8c067406c289fc01ee215
Cahill-99 <ecahill001@gmail.com>
2021-10-13 14:45:52 -0230

    background image fix and text added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import React from 'react'
 function App() {
   return (
     <div className = "white-block">
+      <div className = "text-block">
+        <h2>Something about the site</h2>
+        <p className = "landing-text"> 
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna</p>
+      </div>
       <div className = "green-block">
         <div className = "photo-block">
         </div>
