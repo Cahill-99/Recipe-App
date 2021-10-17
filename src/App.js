4e8e8df713017b14feea1f517be7417951d40a79
Cahill-99 <ecahill001@gmail.com>
2021-10-17 16:39:08 -0230

    logo space added

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from 'react'
 
 function App() {
   return (
-    <div className = "white-block">
-      <div className = "text-block">
-        <h2>Something about the site</h2>
-        <p className = "landing-text"> 
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna</p>
-        <button className = "startbtn">Get Started</button>
-      </div>
-      <div className = "green-block">
-        <div className = "photo-block">
+    <div className = "wrapper">
+      <h3>Logo Name</h3>
+      <div className = "white-block">
+        <div className = "text-block">
+          <h2>Something about the site</h2>
+          <p className = "landing-text"> 
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna</p>
+          <button className = "startbtn">Get Started</button>
+        </div>
+        <div className = "green-block">
+          <div className = "photo-block">
+          </div>
         </div>
       </div>
     </div>
