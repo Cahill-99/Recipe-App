1fc2e55ffbbdfe0d8acab848b07e0ffd60e24c13
Cahill-99 <ecahill001@gmail.com>
2021-10-21 15:25:18 -0230

    Main page basics laid out

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Landing from "./components/Landing";
+import Main from "./components/Main";
 
 function App() {
   return (
     <div className = "wrapper">
       <h3>Logo Name</h3>
-      <Landing />
+      {/*<Landing />*/}
+      <Main />
     </div>
   );
 }
