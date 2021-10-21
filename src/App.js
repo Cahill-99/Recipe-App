b75802b0f846652e602f868b12e706e13fb18cb6
Cahill-99 <ecahill001@gmail.com>
2021-10-21 13:04:46 -0230

    seperated landing page component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import React from 'react'
+import React from 'react';
+import Landing from "./components/Landing";
 
 function App() {
   return (
     <div className = "wrapper">
       <h3>Logo Name</h3>
-      <div className = "white-block">
-        <div className = "text-block">
-          <h2>Something about the site</h2>
-          <p className = "landing-text"> 
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna</p>
-          <button className = "startbtn">Get Started</button>
-        </div>
-        <div className = "green-block">
-          <div className = "photo-block">
-          </div>
-        </div>
-      </div>
+      <Landing />
     </div>
   );
 }
