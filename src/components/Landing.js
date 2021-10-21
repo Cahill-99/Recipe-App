40ca1918ff8f458de132d4365a22d3c0a3dc3bbb
Cahill-99 <ecahill001@gmail.com>
2021-10-21 15:25:43 -0230

    Components

---
+++ src/components/Landing.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+function Landing() {
+    return (
+    <div className = "base-wrapper">
+        <div className = "white-block">
+            <div className = "text-block">
+                <h2>Something about the site</h2>
+                <p className = "landing-text"> 
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna</p>
+                <button className = "startbtn">Get Started</button>
+            </div>
+            <div className = "green-block-landing">
+                <div className = "photo-block">
+                </div>
+            </div>
+        </div>
+    </div>
+    );
+}
+
+export default Landing;
---
+++ src/components/Main.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Main() {
+    return (
+
+    <div className = "base-wrapper-main">
+        <div className = "green-block-main">
+
+        </div>
+        <div className = "blue-block-main">
+            <div className = "white-block-main">
+
+            </div>
+        </div>
+    </div>
+    );
+}
+
+export default Main;
