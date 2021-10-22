04b9e50b80e0b91f9ed528a0393eaf2b39dba3bb
Cahill-99 <ecahill001@gmail.com>
2021-10-22 20:04:54 -0230

    green and blue block components

---
+++ src/components/Blueblk.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Blueblk() {
+    return (
+
+        <div className = "blue-block-main">
+            <div className = "white-block-main">
+            </div>
+        </div>
+    );
+}
+
+export default Blueblk;
---
+++ src/components/Greenblk.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Greenblk() {
+    return (
+
+        <div className = "green-block-main">
+        </div>
+
+    );
+}
+
+export default Greenblk;
