a5621be0c36fa3e33c8e4dd831d5517574e20d66
Cahill-99 <ecahill001@gmail.com>
2021-10-26 11:30:40 -0230

    placeholder plate added

--- src/components/Blueblk.js
+++ src/components/Blueblk.js
@@ -7,6 +7,7 @@ class Blueblk extends React.Component {
 
             <div className = "blue-block-main">
                 <div className = "white-block-main">
+                <img className = "placeholder-plate"  src = "Images/plate.png" alt = "plate with fork"></img>
                 </div>
             </div>
         );
