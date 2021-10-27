e514388904d4e2206637cffaf6abee1679701b72
Cahill-99 <ecahill001@gmail.com>
2021-10-27 12:31:50 -0230

    filter buttons started

--- src/components/Blueblk.js
+++ src/components/Blueblk.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Orangefilters from './Orangefilters';
 
 
 class Blueblk extends React.Component {
@@ -6,6 +7,7 @@ class Blueblk extends React.Component {
         return (
 
             <div className = "blue-block-main">
+                <Orangefilters/>
                 <div className = "white-block-main">
                 <img className = "placeholder-plate"  src = "Images/plate.png" alt = "plate with fork"></img>
                 </div>
