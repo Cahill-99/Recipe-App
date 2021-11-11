3534ca2c07a4c9c8f9b5c2da4d5fd51661f58bf0
Cahill-99 <ecahill001@gmail.com>
2021-11-11 15:04:09 -0330

    api attempt

--- src/components/Greenblk.js
+++ src/components/Greenblk.js
@@ -2,12 +2,33 @@ import React from 'react';
 
 
 class Greenblk extends React.Component {
+
+    state = {
+        recipes: []
+    }
+
+    componentDidMount() {
+        //const APP_ID = "efb4537f";
+        ///const APP_KEY = "3957081447cbddc95fd0ae1dda9e5529"
+        //let baseURL = `https://api.edamam.com/search?q=pizza&app_id=${APP_ID}&app_key=${APP_KEY}`
+    }
+
+    fetchRecipes = () => {
+        const APP_ID = "efb4537f";
+        const APP_KEY = "3957081447cbddc95fd0ae1dda9e5529"
+        fetch(`https://api.edamam.com/search?q=pizza&app_id=${APP_ID}&app_key=${APP_KEY}`)
+        .then((response) => response.json())
+        .then(recipesList => {
+            this.setState({ recipes: recipesList})
+        })
+        console.log(this.state);
+    }
     render() {
 
         return (
 
             <div className = "green-block-main">
-                <h4>Ingredients</h4>
+                <h4 onClick ={this.fetchRecipes} >Ingredients</h4>
                 <form className = "search-form">
                     <input className = "search-bar">
                     </input>
@@ -20,6 +41,14 @@ class Greenblk extends React.Component {
 
         );
     }
+
 }
+const api = () => {
+
+    
+    const APP_ID = "efb4537f";
+    const APP_KEY = "3957081447cbddc95fd0ae1dda9e5529"
+    let baseURL = `https://api.edamam.com/search?q=pizza&app_id=${APP_ID}&app_key=${APP_KEY}`
+    }
 
 export default Greenblk;
--- src/components/Main.js
+++ src/components/Main.js
@@ -21,6 +21,7 @@ const api = () => {
     
     const APP_ID = "efb4537f";
     const APP_KEY = "3957081447cbddc95fd0ae1dda9e5529"
+    let baseURL = `https://api.edamam.com/api/recipes/v2`
     }
 
 export default Main;
