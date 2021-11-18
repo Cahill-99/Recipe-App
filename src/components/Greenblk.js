870670ddd4a4435f88dba10dfe509c22d45f4c14
Cahill-99 <ecahill001@gmail.com>
2021-11-18 14:28:38 -0330

    search function works

--- src/components/Greenblk.js
+++ src/components/Greenblk.js
@@ -6,10 +6,12 @@ class Greenblk extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            search:''
+            search:'',
+            recipes:''
           
 };
-    this.handleChange = this.handleChange.bind(this)
+    this.handleChange = this.handleChange.bind(this);
+    this.fetchRecipes = this.fetchRecipes.bind(this);
       //  this.updateSearch = this.updateSearch.bind(this);
       //  this.checkTyping = this.checkTyping.bind(this);
 
@@ -25,45 +27,29 @@ class Greenblk extends React.Component {
            this._timeout = null;
            this.setState({
               search:val
-           });         console.log(this.state)
+           });
+           this.fetchRecipes(this.state.search);
         },1000);
 
       }
 
 
-    //checkTyping() {
-    //    let timer;
 
-     //   clearTimeout(timer);
 
-      //  timer = setTimeout(() => {
-     //       this.updateSearch()
-      //  }, 3000);
+    fetchRecipes = (search) => {
+        const APP_ID = "efb4537f";
+        const APP_KEY = "3957081447cbddc95fd0ae1dda9e5529";
+        //let query = this.state;
+        console.log(this.state.search);
         
+        fetch(`https://api.edamam.com/search?q=${this.state.search}&app_id=${APP_ID}&app_key=${APP_KEY}`)
+        .then((response) => response.json())
+        .then(recipesList => {
+            this.setState({ recipes: recipesList})
+            console.log(recipesList)
+        })
+    };
 
-    //}
-
-
-  //  updateSearch(event) {
-  //      const input = event.currentTarget.value
-  //      this.setState({search:input});
-  //      console.log("test");
-
-     // }
-
-
-    //fetchRecipes = () => {
-       // const APP_ID = "efb4537f";
-      //  const APP_KEY = "3957081447cbddc95fd0ae1dda9e5529"
-        
-       // fetch(`https://api.edamam.com/search?q=pizza&app_id=${APP_ID}&app_key=${APP_KEY}`)
-       // .then((response) => response.json())
-       // .then(recipesList => {
-       //     this.setState({ recipes: recipesList})
-        //    console.log(recipesList)
-      //  })
-       // console.log(this.state);
-   // };
     render() {
 
         return (
