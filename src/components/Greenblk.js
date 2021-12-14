d5135393fc07620853bc08e30ab4cbfeea212cab
Cahill-99 <ecahill001@gmail.com>
2021-12-14 12:44:44 -0330

    changed Greenblk to function, changed handlechange function

--- src/components/Greenblk.js
+++ src/components/Greenblk.js
@@ -1,94 +1,99 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 
-class Greenblk extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            search:'',
-            recipes:'',
-            suggestions:[],
-            list:[]
-          
-        };
-    this.handleChange = this.handleChange.bind(this);
+function Greenblk(props) {
 
 
-    }
+
+    const [search,setSearch] = useState('')
+    //const [recipes,setRecipes] = useState('')
+    const [suggestions,setSuggestions] = useState([])
+    const [list,setList] = useState([])
+    const [inputValue,setInputValue] = useState('')
+    const [timer,setTimer]  = useState('')
 
 
-    handleChange(evt) {
-        if(this._timeout){ //if there is already a timeout in process cancel it
-            clearTimeout(this._timeout);
-        }
-        const val = evt.target.value;
-        this._timeout = setTimeout(()=>{
-            if(val !== ""){
-           this.autoComplete(val);
-            }
-        },500);
+    const handleChange = e => {
+
+        setInputValue(e.target.value)
+
+        clearTimeout(timer)
+        const val = e.target.value
+        const newTimer = setTimeout(() => {
+            if(val !== "")
+            autoComplete(val)
+        }, 500)
+
+        setTimer(newTimer)
 
       }
 
 
 
-      autoComplete = (search) => {
+      const autoComplete = (search) => {
         const API_KEY = "82138415fc524a1d950b2bc22191c8cc";
         console.log(`search ${search}`);
         fetch(`https://api.spoonacular.com/food/ingredients/autocomplete?query=${search}&number=5&apiKey=${API_KEY}`)
         .then((response) => response.json())
         .then(autoComplete => {
-            this.setState({ suggestions: autoComplete})
+            setSuggestions(autoComplete);
+            //setState({ suggestions: autoComplete})
         })
-        console.log(this.state.suggestions);
+        console.log(suggestions);
     };
 
 
 
-    handleClick = (selected) => {
-        let searchString = this.state.search + " " + selected;
-        let ingredientsList = this.state.list.concat([selected]);
-        this.setState({
-        search: searchString,
-        suggestions: "",
-        list: ingredientsList})
+    const handleClick = (selected) => {
+        let searchString = search + " " + selected;
+        let ingredientsList = list.concat([selected]);
+        setSearch(searchString);
+        setSuggestions("");
+        setList(ingredientsList)
+        // setState({
+        // search: searchString,
+        // suggestions: "",
+        // list: ingredientsList})
 
         console.log(searchString)
         console.log(ingredientsList)
 
-        this.props.fetchRecipes(searchString)
+        props.fetchRecipes(searchString)
     }
     
 
 
 
-    deleteIngredient = (ingredient) => {
+    const deleteIngredient = (ingredient) => {
 
         //remove clicked ingredient from list
-        const list = [...this.state.list];
-        const updatedList = list.filter(item => item !== ingredient);
+        const listCopy = [...list];
+        const updatedList = listCopy.filter(item => item !== ingredient);
 
-        this.setState({list: updatedList});
+        setList(updatedList)
+        //setState({list: updatedList});
 
         //remove clicked ingredient from search state
         const ingredientPiece = " " + ingredient;
-        const prevSearchString = this.state.search;
+        const prevSearchString = search;
         const newSearchString = prevSearchString.replace(ingredientPiece,"")
 
-        this.setState({ search: newSearchString});
-        this.props.fetchRecipes(newSearchString);
+        setSearch(newSearchString)
+        //setState({ search: newSearchString});
+        props.fetchRecipes(newSearchString);
 
         console.log(newSearchString);
     }
 
-    resetSearch = () => { //Remove all ingredients
-        this.setState({list: []})
-        this.setState({search:""})
+    const resetSearch = () => { //Remove all ingredients
+        setList([]);
+        setSearch("")
+        
+        //setState({list: []})
+        //setState({search:""})
         console.log("list reset")
     }
 
-    render() {
 
         return (
 
@@ -96,33 +101,33 @@ class Greenblk extends React.Component {
                 <div className = "green-upper-wrapper">
                     <h4>Ingredients</h4>
                     <form className = "search-form">
-                        <input className = "search-bar" type="text" onChange= {this.handleChange}>
+                        <input className = "search-bar" type="text" value = {inputValue} onChange= {handleChange}>
                         </input>
                     </form>
                     <div className = "green-info-wrapper">
-                        {this.state.suggestions.length === 0 && this.state.list.length === 0 &&(
+                        {suggestions.length === 0 && list.length === 0 &&(
                         <div className = "green-instructions-wrapper">
                         <img className = "green-arrow"  src = "Images/greenarrow.png" alt = "up arrow"></img>
                         <p className = "green-instructions">Add an ingredient to begin your search</p>
 
                         </div>
                         )}
-                        {this.state.suggestions.length !== 0 && (
+                        {suggestions.length !== 0 && (
                         <div className = "auto-complete-dropdown">
-                            {this.state.suggestions.map(hit =>{
+                            {suggestions.map(hit =>{
                                 return (
-                                    <p className = "suggestions" key = {hit.name} onClick = {()=> {this.handleClick(hit.name)}}>{hit.name}</p>
+                                    <p className = "suggestions" key = {hit.name} onClick = {()=> {handleClick(hit.name)}}>{hit.name}</p>
                                 )
                             })}
                         </div>
                         )}
-                        {this.state.list.length !== 0 &&(
+                        {list.length !== 0 &&(
                             <div className="ing-list">
-                                {this.state.list.map(ing => {
+                                {list.map(ing => {
                                     return (
                                         <div key={ing} className="ing-wrapper">
                                             <li className="ingredient">{ing}</li>
-                                            <button onClick={() => this.deleteIngredient(ing)} className="ing-x-button">X</button>
+                                            <button onClick={() => deleteIngredient(ing)} className="ing-x-button">X</button>
                                         </div>
                                     );
                                 })}
@@ -130,8 +135,8 @@ class Greenblk extends React.Component {
                         )}
                     </div>
                 </div>
-                {this.state.list.length !== 0 &&(
-                    <button className = "reset-button" onClick={() => this.resetSearch()}>Reset</button>
+                {list.length !== 0 &&(
+                    <button className = "reset-button" onClick={() => resetSearch()}>Reset</button>
                 )}
 
             </div>
@@ -139,5 +144,4 @@ class Greenblk extends React.Component {
         );
     }
 
-};
 export default Greenblk;
