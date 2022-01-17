d151854700f837aa213d3b827e722b0b816627a3
Cahill-99 <ecahill001@gmail.com>
2022-01-17 17:10:35 -0330

    infinite scroll attempt 2

--- src/components/Blueblk.js
+++ src/components/Blueblk.js
@@ -1,6 +1,8 @@
 import React from 'react';
 // import InfiniteScroll from 'react-infinite-scroll-component';
-import useInfiniteScroll from 'react-infinite-scroll-hook';
+import { useInfiniteScrollHook } from 'use-infinite-scroll-hook/lib';
+import { useRef } from 'react';
+//import useInfiniteScroll from 'react-infinite-scroll-hook';
 import Orangefilters from './Orangefilters';
 
 
@@ -9,16 +11,22 @@ import Orangefilters from './Orangefilters';
 
 function Blueblk(props) {
 
+    const targetContainer = useRef<"white-block-main">(null);
 
-        const { loading, items, hasNextPage, error, loadMore } = useLoadItems();
+    const loadData = () => console.log('load');
+  
+    useInfiniteScrollHook(targetContainer, loadData);
 
-        const [sentryRef, { rootRef }] = useInfiniteScroll({
-            loading,
-            hasNextPage,
-            onLoadMore: loadMore,
-            disabled: !!error,
-            rootMargin: '0px 0px 400px 0px',
-          });
+
+        // const { loading, items, hasNextPage, error, loadMore } = useLoadItems();
+
+        // const [sentryRef, { rootRef }] = useInfiniteScroll({
+        //     loading,
+        //     hasNextPage,
+        //     onLoadMore: loadMore,
+        //     disabled: !!error,
+        //     rootMargin: '0px 0px 400px 0px',
+        //   });
 
 
         return (
@@ -33,47 +41,44 @@ function Blueblk(props) {
                 dropdown={props.dropdown}
                 handleTime={props.handleTime}/>
                 
-                <div className = "white-block-main" ref = {rootRef}>
+                <div className = "white-block-main" useref = {targetContainer}>
                 {/* <InfiniteScroll dataLength={props.recipes.length} next={props.loadMore} hasMore={true} loader={<h5>Loading...</h5>} scrollableTarget="recipes-wrapper"> */}
                     {/* <div className = "recipes-wrapper"> */}
 
-                        <ListContainer
-        // This where we set our scrollable root component.
-        ref={rootRef}
-        >
-        <List>
 
-            {props.recipes && props.recipes.map((recipeItem, index) => {
-                return  (
-                    <ListItem key={item.key}>{
-                        <div className = "recipe-card" key = {index} ref={sentryRef}>
-                            <img className = "recipe-image" src = {recipeItem.recipe.image} alt = {recipeItem.recipe.label}></img>
-                            <div className = "recipe-card-title-bar">
-                                <p key={index} className = "card-text">{recipeItem.recipe.label}</p>
-                            </div>
-                            <div className = "recipe-card-icon-bar">
-                                <div className = "time-display">
-                                <img className = "clock-logo" src = "Images/clock.png" alt = "time icon"></img>
-                                <p className = "card-icon-text">{recipeItem.recipe.totalTime} min </p>
+
+
+                {props.recipes && props.recipes.map((recipeItem, index) => {
+                    return  (
+
+                            <div className = "recipe-card" key = {index}>
+                                <img className = "recipe-image" src = {recipeItem.recipe.image} alt = {recipeItem.recipe.label}></img>
+                                <div className = "recipe-card-title-bar">
+                                    <p key={index} className = "card-text">{recipeItem.recipe.label}</p>
                                 </div>
-                                <div className = "time-display">
-                                <img className = "diet-logo" src = "Images/diet.png" alt = "time icon"></img>
-                                <p className = "card-icon-text">test</p>
+                                <div className = "recipe-card-icon-bar">
+                                    <div className = "time-display">
+                                    <img className = "clock-logo" src = "Images/clock.png" alt = "time icon"></img>
+                                    <p className = "card-icon-text">{recipeItem.recipe.totalTime} min </p>
+                                    </div>
+                                    <div className = "time-display">
+                                    <img className = "diet-logo" src = "Images/diet.png" alt = "time icon"></img>
+                                    <p className = "card-icon-text">test</p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>}
-                    </ListItem>
+
+                    )}
+                    
                 )}
-                
-            )}
 
-            {(loading || hasNextPage) && (
-            <ListItem ref={sentryRef}>
-                <Loading />
-            </ListItem>
-            )}
-        </List>
-        </ListContainer>
+                {/* {(loading || hasNextPage) && (
+                <ListItem ref={sentryRef}>
+                    <Loading />
+                </ListItem>
+                )}
+            </List>
+        </ListContainer> */}
 
 
 
