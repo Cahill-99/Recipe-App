806a94669fefc4b50f6506e1001a2b917a9bb527
Cahill-99 <ecahill001@gmail.com>
2022-01-23 20:12:37 -0330

    results page component seperated

--- src/components/Blueblk.js
+++ src/components/Blueblk.js
@@ -1,7 +1,6 @@
-import React, {useState} from 'react';
-import { useInfiniteScrollHook } from 'use-infinite-scroll-hook/lib';
-import { useRef } from 'react';
+import React from 'react';
 import Orangefilters from './Orangefilters';
+import Results from './Results';
 
 
 
@@ -9,20 +8,7 @@ import Orangefilters from './Orangefilters';
 
 function Blueblk(props) {
 
-    const [recipeLimit,setRecipeLimit] = useState(10)
 
-    const showMore = () => {
-            setRecipeLimit(recipeLimit + 10)
-            console.log("should be loading more")
-
-
-    }
-
-    const targetContainer = useRef(null);
-
-    const loadData = () => showMore();
-  
-    useInfiniteScrollHook(targetContainer, loadData);
 
 
 
@@ -30,6 +16,8 @@ function Blueblk(props) {
         return (
 
             <div className = "blue-block-main">
+                <Results recipes={props.recipes}/>
+
                 <Orangefilters toggleGlutenFilter={props.toggleGlutenFilter} 
                 glutenFreeColor={props.glutenFreeColor}
                 toggleVegetarianFilter={props.toggleVegetarianFilter}
@@ -39,46 +27,7 @@ function Blueblk(props) {
                 dropdown={props.dropdown}
                 handleTime={props.handleTime}/>
                 
-                <div className = "white-block-main" ref = {targetContainer}>
 
-
-
-
-                {props.recipes && props.recipes.slice(0,recipeLimit).map((recipeItem, index) => {
-                    return  (
-
-                            <div className = "recipe-card" key = {index}>
-                                <img className = "recipe-image" src = {recipeItem.recipe.image} alt = {recipeItem.recipe.label}></img>
-                                <div className = "recipe-card-title-bar">
-                                    <p key={index} className = "card-text">{recipeItem.recipe.label}</p>
-                                </div>
-                                <div className = "recipe-card-icon-bar">
-                                    <div className = "time-display">
-                                    <img className = "clock-logo" src = "Images/clock.png" alt = "time icon"></img>
-                                    <p className = "card-icon-text">{recipeItem.recipe.totalTime} min </p>
-                                    </div>
-                                    <div className = "time-display">
-                                    <img className = "diet-logo" src = "Images/diet.png" alt = "time icon"></img>
-                                    <p className = "card-icon-text">test</p>
-                                    </div>
-                                </div>
-                            </div>
-
-                    )}
-                    
-                )}
-
-
-
-                    {props.recipes.length === 0 && (
-
-                        <img className = "placeholder-plate"  src = "Images/plate.png" alt = "plate with fork"></img>
-
-                        )}
-                
-
-
-                </div>
             </div>
         );
     }
